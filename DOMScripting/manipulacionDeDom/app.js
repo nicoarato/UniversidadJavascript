3c5dd7cfed3a1223a3e3245595680c6ddf8426ff
Nico Arato <nico.arato@gmail.com>
2021-02-14 02:18:51 -0300

    Manipulación de dom 1.

---
+++ DOMScripting/manipulacionDeDom/app.js
@@ -0,0 +1,18 @@
+document.write('Saludos desde JS')
+
+function mostrar() {
+    document.write('Hola mundo')
+}
+
+function mostrar2() {
+
+    document.getElementById('titulo').innerHTML = 'Nuevo titulo';
+}
+
+function mostrar3() {
+    document.getElementById('imagen').src = 'javascript.png';
+}
+
+function mostrar4() {
+    document.getElementById('titulo').style.color = 'blue';
+}
